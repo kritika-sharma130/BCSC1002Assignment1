6c28326a57644461fdc7b7cb7bcce073d1e6b224
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-28 19:23:34 +0530

    add components of Library.java class

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -6,5 +6,47 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] namesOfBooksAvailable;
+
+    public Library() {
+        this.namesOfBooksAvailable = new Book[8];
+        for (int i = 0; i < namesOfBooksAvailable.length; i++) {
+            namesOfBooksAvailable[i] = new Book();
+        }
+    }
+
+    public Library(Book[] namesOfBooksAvailable) {
+        this.namesOfBooksAvailable = namesOfBooksAvailable;
+    }
+
+    public Book[] getNamesOfBooksAvailable() {
+        return namesOfBooksAvailable.clone();
+    }
+
+    public void setNamesOfBooksAvailable(Book[] namesOfBooksAvailable) {
+        this.namesOfBooksAvailable = namesOfBooksAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "namesOfBooksAvailable=" + Arrays.toString(namesOfBooksAvailable) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getNamesOfBooksAvailable(), library.getNamesOfBooksAvailable());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getNamesOfBooksAvailable());
+    }
 }
