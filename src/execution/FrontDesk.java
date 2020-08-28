35b24fa835fd3fc6f41ed6cd88d3a1a29ddad1fb
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-28 19:50:19 +0530

    create menu for FrontDesk.java class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,37 @@
  * */
 package execution;
 
-public class FrontDesk {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class FrontDesk {
+    private static final int ISSUE_A_BOOK = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 2;
+    private static final int SHOW_ALL_THE_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int studentInput;
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice (1..4): ");
+            studentInput = scanner.nextInt();
+            switch (studentInput) {
+                case ISSUE_A_BOOK:
+                    break;
+                case RETURN_A_PREVIOUSLY_ISSUED_BOOK:
+                    break;
+                case SHOW_ALL_THE_ISSUED_BOOKS:
+                    break;
+                default:
+                    break;
+            }
+        } while (studentInput != EXIT);
+        scanner.close();
     }
 }
