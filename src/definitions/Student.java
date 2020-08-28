fa152e19f2e95e5b56d3e0b0e222adf9aaf52064
kritika-sharma130 <kritika.sharma_cs19@gla.ac.in>
2020-08-28 18:47:51 +0530

    adding author information for Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,7 +1,7 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Kritika Sharma
+ *  Date: 28/08/20
+ *  Time: 6:49 PM
  *  File Name : Student.java
  * */
 package definitions;
